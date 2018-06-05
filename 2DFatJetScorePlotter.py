65d30d328cd60833704fca074b790768eea9045b
Alexander Titterton <alextitterton@physics.org>
2018-06-05 10:49:01 +0100

    added 2D discriminator plot

--- 2DFatJetScorePlotter.py
+++ 2DFatJetScorePlotter.py
@@ -18,10 +18,11 @@ import warnings
 
 #Get Options
 
-parser = a.ArgumentParser(description='2D Fat Jet Mass plot')
+parser = a.ArgumentParser(description='2D Fat Jet Score plot')
 parser.add_argument('-f', '--files', nargs='*', required=True, help='Path to dataframe file(s) from ROOTCuts')
 parser.add_argument('--HT', type=float, default=0., help='Apply minimum HT cut, default = 0')
-parser.add_argument('--discrim', type=float, default=0.3, help='Minimum double-b-tag discriminator output, default is 0.3')
+parser.add_argument('--minMass', type=float, default=0., help='Minimum fat jet mass, default is 0.')
+parser.add_argument('--maxMass', type=float, default=999999., help='Maximum fat jet mass, default is 999999.')
 parser.add_argument('--type', default='Signal', help='Type of sample: e.g. Signal, TTJets, QCD etc')
 parser.add_argument('-x', '--NoX', action='store_true', help='This argument suppresses showing plots via X-forwarding')
 parser.add_argument('-o', '--NoOutput', action='store_true', help='This argument suppresses the output of PDF plots')
@@ -36,7 +37,7 @@ for file in args.files:
 df = pd.concat(df_list)
 
 #Make the output directories
-directory = '2DFatJetMass_{0}'.format(args.type)
+directory = '2DFatJetScore_{0}'.format(args.type)
 temp_dir = directory
 suffix = 1
 while os.path.exists(temp_dir):
@@ -47,33 +48,32 @@ if not args.NoOutput:
     os.makedirs(temp_dir)
 
 sns.set_style("white")
-discrim = args.discrim
 
-df = df.loc[((df['FatDoubleBJetA_mass'] < 200.) & (df['FatDoubleBJetB_mass'] < 200.) & (df['FatDoubleBJetA_mass'] > 0.) & (df['FatDoubleBJetB_mass'] > 0.) & (df['FatDoubleBJetA_discrim'] > discrim) & (df['FatDoubleBJetB_discrim'] > discrim) & (df['HT'] > args.HT))]
+df = df.loc[((df['FatDoubleBJetA_mass'] < args.maxMass) & (df['FatDoubleBJetB_mass'] < args.maxMass) & (df['FatDoubleBJetA_mass'] > args.minMass) & (df['FatDoubleBJetB_mass'] > args.minMass) & (df['HT'] > args.HT))]
 
-g = sns.JointGrid(x=df['FatDoubleBJetA_mass'], y=df['FatDoubleBJetB_mass'], space=0.)
+g = sns.JointGrid(x=df['FatDoubleBJetA_discrim'], y=df['FatDoubleBJetB_discrim'], space=0.)
 g.plot_joint(plt.hexbin, norm=LogNorm(), cmap=args.cmap, gridsize=30)
 
 cm = plt.cm.get_cmap(args.cmap)
 
-nx, binsx, patchesx = g.ax_marg_x.hist(df['FatDoubleBJetA_mass'], log=True, bins=30)
+nx, binsx, patchesx = g.ax_marg_x.hist(df['FatDoubleBJetA_discrim'], log=True, bins=30)
 colx = 1.5*(nx-nx.min())/(nx.max()-nx.min())
 for c, p in zip(colx, patchesx):
     plt.setp(p, 'facecolor', cm(c))
 
-ny, binsy, patchesy = g.ax_marg_y.hist(df['FatDoubleBJetB_mass'], bins=30, log=True, orientation='horizontal')
+ny, binsy, patchesy = g.ax_marg_y.hist(df['FatDoubleBJetB_discrim'], bins=30, log=True, orientation='horizontal')
 coly = 1.5*(ny-ny.min())/(ny.max()-ny.min())
 for c, p in zip(coly, patchesy):
     plt.setp(p, 'facecolor', cm(c))
 
-g.set_axis_labels('AK8 Double-$b$-tagged Jet A SoftDrop Mass', 'AK8 Double-$b$-tagged Jet B SoftDrop Mass', fontsize=14)
+g.set_axis_labels('AK8 Fat Jet A Double-$b$-tag Score', 'AK8 Fat Jet A Double-$b$-tag Score', fontsize=14)
 
 sns.plt.subplots_adjust(left=0.15, right=0.9, top=0.9, bottom=0.15)
 cax = g.fig.add_axes([.91, .3, .02, .3])  # x, y, width, height
 sns.plt.colorbar(cax=cax)
 
 if not args.NoOutput:
-    plt.savefig(os.path.join(temp_dir, '2DFatJetMass.pdf'))
-    print('Saved 2DFatJetMass.pdf output file')
+    plt.savefig(os.path.join(temp_dir, '2DFatJetScore.pdf'))
+    print('Saved 2DFatJetScore.pdf output file')
 if not args.NoX:
     plt.show()
