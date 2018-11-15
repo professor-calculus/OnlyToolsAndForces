887e9d11e54895e9f9328d9d4cd82a8557948e6d
Alexander Titterton <alextitterton@physics.org>
2018-11-15 11:53:00 +0000

    Script to obtain strength parameters from HiggsCombine root files

---
+++ mu_getter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import uproot
+import argparse as a
+
+parser = a.ArgumentParser(description='Thing to get mu values from HiggsCombine output')
+parser.add_argument('-f', '--files', nargs='*', required=True, help='Path to HiggsCombine output root files')
+args = parser.parse_args()
+
+Msq = []
+Mlsp = []
+mu_obs = []
+mu_exp = []
+
+for file in args.files:
+
+    temp_Msq = float(file.split('_')[1][:-2])
+    R = file.split('_')[2][:-5]
+    Msq.append(temp_Msq)
+    if R == 'R0p99':
+        Mlsp.append(1.16)
+    elif R == 'R0p555':
+        Mlsp.append(100.)
+    elif R == 'R0p384':
+        Mlsp.append(200.)
+
+
+    f = uproot.open(file)['limit']
+    for x in f.iterate(['limit'], outputtype=tuple):
+        for y in x:
+            mu_exp.append(y[2])
+            mu_obs.append(y[5])
+
+df_exp = pd.DataFrame({
+    'Msq': Msq,
+    'Mlsp': Mlsp,
+    'mu': mu_exp,
+    })
+
+df_obs = pd.DataFrame({
+    'Msq': Msq,
+    'Mlsp': Mlsp,
+    'mu': mu_obs,
+    })
+
+print('Observed mu values:')
+print(df_obs)
+
+print('Expected mu values:')
+print(df_exp)
+
+df_obs.to_csv('mu_observed.txt', header=False, index=False, sep='\t', columns=['Msq', 'Mlsp', 'mu'])
+df_exp.to_csv('mu_expected.txt', header=False, index=False, sep='\t', columns=['Msq', 'Mlsp', 'mu'])
--- signal_plotting_scripts.py
+++ signal_plotting_scripts.py
@@ -2,36 +2,45 @@
 
 2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal
 2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal --nHiggs2bb
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 2b1mu
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 0b1mu
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 2mu
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 0b2mu
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 1b1mu
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 1b2mu
 
 2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500
 2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --nHiggs2bb
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 2b1mu --HT 1500
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 0b1mu --HT 1500
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 2mu --HT 1500
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 0b2mu --HT 1500
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 1b1mu --HT 1500
-2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region 1b2mu --HT 1500
+
+2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500
+2DFatJetScorePlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --nHiggs2bb
 
 2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --minDiscrim 0.3
 2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --nHiggs2bb --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 2b1mu --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 0b1mu --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 2mu --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 0b2mu --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 1b1mu --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 1b2mu --minDiscrim 0.3
 
 2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --minDiscrim 0.3
 2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --nHiggs2bb --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 2b1mu --HT 1500 --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 0b1mu --HT 1500 --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 2mu --HT 1500 --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 0b2mu --HT 1500 --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 1b1mu --HT 1500 --minDiscrim 0.3
-2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region 1b2mu --HT 1500 --minDiscrim 0.3
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --minDiscrim 0.6
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --nHiggs2bb --minDiscrim 0.6
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --minDiscrim 0.8
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --nHiggs2bb --minDiscrim 0.8
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --minDiscrim 0.9
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --nHiggs2bb --minDiscrim 0.9
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --minDiscrim 0.6
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --nHiggs2bb --minDiscrim 0.6
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --minDiscrim 0.8
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --nHiggs2bb --minDiscrim 0.8
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --minDiscrim 0.9
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 1500 --nHiggs2bb --minDiscrim 0.9
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --minDiscrim 0.3
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --nHiggs2bb --minDiscrim 0.3
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --minDiscrim 0.6
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --nHiggs2bb --minDiscrim 0.6
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --minDiscrim 0.8
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --nHiggs2bb --minDiscrim 0.8
+
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --minDiscrim 0.9
+2DFatJetMassPlotter.py -f ROOTAnalysis.txt -x --region Signal --HT 3500 --nHiggs2bb --minDiscrim 0.9
